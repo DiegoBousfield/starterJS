c62522209a811c2a6871be7f196fac4ccc537b92
Diego Bousfield <diegobousfield@gmail.com>
2018-09-21 16:07:09 -0300

    Add deleteTodo function
    Update the renderTodo function to work with the deleteTodo function

--- JS/todo/js/index.js
+++ JS/todo/js/index.js
@@ -8,10 +8,18 @@ function renderTodo() {
   listElement.innerHTML = '';
 
   for(todo of todos) {
-    var todoElement = document.createElement('li');
-    var todoText = document.createTextNode(todo);
+    var todoElement    = document.createElement('li');
+    var todoText       = document.createTextNode(todo);
+    var deleteElement  = document.createElement('a'); 
+    var deleteText     = document.createTextNode(' Excluir');
 
+    deleteElement.appendChild(deleteText);
+    deleteElement.setAttribute('href', '#');
+
+    var pos = todos.indexOf(todo);
+    deleteElement.setAttribute('onclick','deleteTodo('+ pos +')');
     todoElement.appendChild(todoText);
+    todoElement.appendChild(deleteElement);
     listElement.appendChild(todoElement);
   }
 }
@@ -20,6 +28,11 @@ function addTodo() {
   var todoText = inputElement.value;
   todos.push(todoText);
   inputElement.value = '';
+  renderTodo();
+} 
+
+function deleteTodo(pos) {
+  todos.splice(pos, 1);
   renderTodo();
 }
 
