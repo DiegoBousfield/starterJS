83e1888a85560bd3b033f44b50f305c874895d7d
Diego Bousfield <diegobousfield@gmail.com>
2018-09-21 16:53:15 -0300

    Add saveToLocalStorage function

--- JS/todo/js/index.js
+++ JS/todo/js/index.js
@@ -1,8 +1,7 @@
 var listElement   = document.querySelector('#app ul');
 var inputElement  = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
-
-var todos = [];
+var todos         = JSON.parse(localStorage.getItem('todosList')) || [];
 
 function renderTodo() {
   listElement.innerHTML = '';
@@ -12,11 +11,10 @@ function renderTodo() {
     var todoText       = document.createTextNode(todo);
     var deleteElement  = document.createElement('a'); 
     var deleteText     = document.createTextNode(' Excluir');
+    var pos            = todos.indexOf(todo);
 
     deleteElement.appendChild(deleteText);
     deleteElement.setAttribute('href', '#');
-
-    var pos = todos.indexOf(todo);
     deleteElement.setAttribute('onclick','deleteTodo('+ pos +')');
     todoElement.appendChild(todoText);
     todoElement.appendChild(deleteElement);
@@ -29,11 +27,18 @@ function addTodo() {
   todos.push(todoText);
   inputElement.value = '';
   renderTodo();
+  saveToLocalStorage();
 } 
 
 function deleteTodo(pos) {
   todos.splice(pos, 1);
   renderTodo();
+  saveToLocalStorage();
+}
+
+
+function saveToLocalStorage() {
+  localStorage.setItem('todosList', JSON.stringify(todos));  
 }
 
 buttonElement.onclick = addTodo;
