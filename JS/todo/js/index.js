a242e238db153ddcd623c7a6620f9a2cbe2c3e20
Diego Bousfield <diegobousfield@gmail.com>
2018-09-21 15:16:35 -0300

    Add renderTodo function

--- JS/todo/js/index.js
+++ JS/todo/js/index.js
@@ -2,3 +2,18 @@ var listElement   = document.querySelector('#app ul');
 var inputElement  = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
+var todos = [];
+
+function renderTodo() {
+  listElement.innerHTML = '';
+  
+  for(todo of todos) {
+    var todoElement = document.createElement('li');
+    var todoText = document.createTextNode(todo);
+
+    todoElement.appendChild(todoText);
+    listElement.appendChild(todoElement);
+  }
+}
+
+renderTodo();
